6d1b3292b61fae100a224cb1965549344363f796
Crowdbotics <engineering@crowdbotics.com>
2022-12-15 18:36:44 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/studioscreen_dev_50843/MainActivity.java
@@ -0,0 +1,15 @@
+package com.studioscreen_dev_50843;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "studioscreen_dev_50843";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "studioscreen-dev-50843",
+  url: "https://studioscreen-dev-50843.botics.co",
+  api: "https://studioscreen-dev-50843.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
